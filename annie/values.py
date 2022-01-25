ba3d8d6494e3f97349a80ee029ef1f31278cd1fc
Harry Lees <harry.lees@gmail.com>
2022-01-25 21:47:34 +0000

    added constant values

---
+++ annie/values.py
@@ -0,0 +1,17 @@
+ROUTES = ('americas', 'asia', 'europe')
+REGIONS = (
+    'eu',
+    'euw',
+    'eune',
+    'jp',
+    'kr',
+    'lan',
+    'las',
+    'br',
+    'na',
+    'oce',
+    'ru',
+    'tr',
+    'latam',
+    'ap'
+)
